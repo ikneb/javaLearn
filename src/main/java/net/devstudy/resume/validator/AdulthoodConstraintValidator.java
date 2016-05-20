0777fc533a8d8f3bf4911e7cea6c2ba59726f4ec
ikneb <ikneblis@gmail.com>
2016-05-20 21:25:54 +0300

    added validation format date

--- src/main/java/net/devstudy/resume/validator/AdulthoodConstraintValidator.java
+++ src/main/java/net/devstudy/resume/validator/AdulthoodConstraintValidator.java
@@ -1,33 +1,54 @@
 package net.devstudy.resume.validator;
 
 import java.sql.Date;
+import java.text.DateFormat;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
 import net.devstudy.resume.annotation.constraints.Adulthood;
 
-public class AdulthoodConstraintValidator implements ConstraintValidator<Adulthood, Date>{
+public class AdulthoodConstraintValidator implements ConstraintValidator<Adulthood, Date> {
 
 	int adulthoodAge;
 	
+
 	@Override
 	public void initialize(Adulthood constraintAnnotation) {
 		this.adulthoodAge = constraintAnnotation.adulthoodAge();
-		
+
 	}
 
 	@SuppressWarnings("deprecation")
 	@Override
 	public boolean isValid(Date value, ConstraintValidatorContext context) {
-		if(value == null) {
+		java.util.Date nowDate = new java.util.Date();
+		
+		if (value == null) {
 			return true;
 		}
-		java.util.Date nowDate = new java.util.Date();
-		if((nowDate.getYear() - value.getYear()) < adulthoodAge){
-		return false;
+		
+		if ((nowDate.getYear() - value.getYear()) < adulthoodAge || isValidDate(value)) {
+			return false;
 		}
 		return true;
+	}
+
+	public static boolean isValidDate(Date date) {
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+		DateFormat dateFormat = DateFormat.getDateInstance();
+		String str = dateFormat.format(date);
+		try {
+			sdf.parse(str);
+			return true;
+
+		} catch (ParseException e) {
+			return false;
+		}
+
+		
 	}
 
 }
